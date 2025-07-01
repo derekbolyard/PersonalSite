da5a8d6988ddac0317c724fffa07d4aeea4362a5
derekbolyard <32370477+derekbolyard@users.noreply.github.com>
2025-07-01 04:06:17 -0400

    Updated AppRouter.tsx

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -9,11 +9,6 @@ export default function AppRouter() {
       <Routes>
         <Route path="/"          element={<PortfolioPage />} />
         <Route path="/services"  element={<ServicesPage />} />
-        <Route path="/eyes"      element={<EyeTracker   />} />
-        <Route path="/terminal"  element={<RetroTerminal />} />
-        <Route path="/glitch"    element={<GlitchArt     />} />
-        <Route path="/space"     element={<SpaceExplorer />} />
-        <Route path="/doom"      element={<DoomGame      />} />
         <Route path="/design"    element={<DesignToolApp />} />
         {/* add 404 fallback if you like */}
       </Routes>
