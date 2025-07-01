2958a44675c32c7f63a8b730627d43c8104493c1
derekbolyard <32370477+derekbolyard@users.noreply.github.com>
2025-07-01 04:05:23 -0400

    Updated AppRouter.tsx

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,11 +2,6 @@
 import { Routes, Route } from 'react-router-dom';
 import PortfolioPage   from './pages/PortfolioPage';
 import ServicesPage    from './pages/ServicesPage';
-import EyeTracker      from './components/EyeTracker';
-import RetroTerminal   from './components/RetroTerminal';
-import SpaceExplorer   from './components/SpaceExplorer';
-import GlitchArt       from './components/GlitchArt';
-import DoomGame        from './components/DoomGame';
 import DesignToolApp   from './DesignToolApp';
 
 export default function AppRouter() {
